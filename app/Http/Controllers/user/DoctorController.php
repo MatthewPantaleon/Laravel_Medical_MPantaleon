f837f5ddf190fac72255cf35fe3e571b5f79d38c
MatthewPantaleon <45208898+MatthewPantaleon@users.noreply.github.com>
2018-12-13 11:40:21 +0000

    Route Understanding

---
+++ app/Http/Controllers/user/DoctorController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\user;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return 'this is the user doctors';
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show($id)
+    {
+        //
+    }
+
+    
+    public function edit($id)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/home/Test', 'PageController@index');
+
+Route::resource('admin/doctors', 'admin\DoctorController', array("as" => "admin"));
+Route::resource('user/doctors', 'user\DoctorController', array("as" => "user"));
+
